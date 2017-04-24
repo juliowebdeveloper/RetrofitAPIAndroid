f7da630717b3b58aa54c2fa95d31917882b136d5
Julio <juliowebdeveloper@hotmail.com>
2017-04-24 15:57:13 -0300

    Added Java 8 Support

--- app/src/main/java/com/shido/retrofitapiandroid/MainActivity.java
+++ app/src/main/java/com/shido/retrofitapiandroid/MainActivity.java
@@ -1,13 +1,19 @@
 package com.shido.retrofitapiandroid;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.shido.retrofitapiandroid.interfaces.GitHubService;
 import com.shido.retrofitapiandroid.interfaces.UserService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -15,10 +21,19 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private List<GitHubRepo> gits;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView t = (TextView) findViewById(R.id.hello);
+        t.setOnClickListener(v -> {
+           // Toast.makeText(this, "Teste", Toast.LENGTH_SHORT).show();
+            streamGits(gits);
+        });
+        //final List<GitHubRepo> gits = new ArrayList<GitHubRepo>();
 
         //Change base url to Github API
         ServiceGenerator.changeApiBaseUrl("https://api.github.com/");
@@ -34,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
                     for(GitHubRepo repo:response.body()){
                         Log.e("Repo:", repo.getName() + " (ID: " + repo.getId() + " )");
                     }
+                    gits = response.body();
                 }else{
                     Log.e("REQUEST FAILED:", "Cannot request Github repositories");
                 }
@@ -44,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("Error fetching repos", t.getMessage());
             }
         });
+
+
 
 
         UserService userService = ServiceGenerator.createService(UserService.class);
@@ -64,4 +82,37 @@ appends the endpoint url to the base url.*/
 
 
     }
+
+
+    @TargetApi(Build.VERSION_CODES.N)
+    public void streamTeste(){
+        List<String> strings = new ArrayList<>();
+        strings.add("Ola"); strings.add("Oi"); strings.add("He");
+        strings.add("Hello");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+
+            int sum = strings.stream().filter(b -> b.contains("he")).mapToInt(b -> b.length()).sum();
+            Log.e("Log", String.valueOf(sum));
+        }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.N)
+    public void streamGits(List<GitHubRepo> gits){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Stream<GitHubRepo> streamGit = gits.stream();
+            Stream<GitHubRepo> streamGit2 = gits.stream();
+           int sum = streamGit.filter(gitHubRepo ->
+                    gitHubRepo.getName().contains("Broad"))
+                    .mapToInt(gitHubRepo -> gitHubRepo.getId()).sum();
+            //Predicate<GitHubRepo> predicate = s -> s.getName().contains("Broad");
+            long count = streamGit2.filter(s -> s.getName().contains("Broad")).count();
+
+            Log.e("Log", String.valueOf(sum)); //Soma dos Ids cujo nome contem int
+            Toast.makeText(this, "Count " + count, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Sum " + sum, Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 }
